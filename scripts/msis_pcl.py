cac902c4663474f14c1137ea4728a30a17a9db1e
tjsos <tony.jacob@uri.edu>
2023-06-20 15:21:35 -0400

    fix:well defined constructor

--- scripts/msis_pcl.py
+++ scripts/msis_pcl.py
@@ -15,22 +15,11 @@ class MSIS_PCL:
         self.prev = np.zeros((self.number_of_bins,360)).astype(np.float32)
         self.current = np.zeros((self.number_of_bins,360)).astype(np.float32)
 
-    def image_Cb(self, msg):
-        bridge = CvBridge()
-        #GrayScale Image with Bins x 360
-        self.current = bridge.imgmsg_to_cv2(msg)
-        self.diff = (self.prev - self.current)
-        self.height, self.width = self.diff.shape
-        self.prev = self.current
-        
-        self.generate_pointclouds()
-
-    def generate_pointclouds(self):
-        pointcloud_msg = PointCloud2()
+        self.pointcloud_msg = PointCloud2()
         # Set the header information
-        pointcloud_msg.header = Header()
-        pointcloud_msg.header.stamp = rospy.Time.now()
-        pointcloud_msg.header.frame_id = "wamv/msis"
+        self.pointcloud_msg.header = Header()
+        self.pointcloud_msg.header.stamp = rospy.Time.now()
+        self.pointcloud_msg.header.frame_id = "wamv/msis"
 
         # Define the point fields (attributes)
         fields = [
@@ -40,53 +29,61 @@ class MSIS_PCL:
         PointField('intensity', 12, PointField.FLOAT32,1),
         # PointField('rgb', 16, PointField.INT32,1),
         ]
-        pointcloud_msg.fields = fields
+        self.pointcloud_msg.fields = fields
 
         # Set the point cloud dimensions (width and height)
-        pointcloud_msg.width = self.height #HOW MANY POINTS in total (range)
-        pointcloud_msg.height = 1
-        pointcloud_msg.point_step = 4 * (len(fields))  # Each point occupies 16 bytes
+        self.pointcloud_msg.width = self.number_of_bins #HOW MANY POINTS in total (range)
+        self.pointcloud_msg.height = 1
+        self.pointcloud_msg.point_step = 4 * (len(fields))  # Each point occupies 16 bytes
 
         # Populate the point data
-        num_points = pointcloud_msg.width * pointcloud_msg.height
-        pointcloud_msg.row_step = pointcloud_msg.point_step * num_points
-        pointcloud_msg.is_dense = True  # All points are valid
+        num_points = self.pointcloud_msg.width * self.pointcloud_msg.height
+        self.pointcloud_msg.row_step = self.pointcloud_msg.point_step * num_points
+        self.pointcloud_msg.is_dense = True  # All points are valid
 
-        x = np.linspace(self.range_min,self.range_max,self.number_of_bins)
+        self.x = np.linspace(self.range_min,self.range_max,self.number_of_bins)
         #height is number of bins ie no of points. 4 attributes :x,y,z,intensity
-        points = np.zeros((self.number_of_bins,len(fields)), dtype=np.float32)
-        
-        row_values = self.get_middle_row_intensities(self.diff)
+        self.points = np.zeros((self.number_of_bins,len(fields)), dtype=np.float32)
 
+    def image_Cb(self, msg):
+        bridge = CvBridge()
+        #GrayScale Image with Bins x 360
+        self.current = bridge.imgmsg_to_cv2(msg)
+        self.diff = (self.prev - self.current)
+        self.height, self.width = self.diff.shape
+        self.prev = self.current
+        self.generate_pointclouds()
+
+    def generate_pointclouds(self):
+        row_values = self.get_middle_row_intensities(self.diff)
         for angle in range(len(row_values)):
             if row_values[angle]!=0:
                 theta = 180-angle
-                for i in range(len(x)): #number of bins
+                for i in range(len(self.x)): #number of bins
                     # Add X values
-                    points[:][i][0] = x[i] * np.cos(np.deg2rad(theta))
+                    self.points[:][i][0] = self.x[i] * np.cos(np.deg2rad(theta))
                     
                     # Add Y Value
-                    points[:][i][1] = x[i] * np.sin(np.deg2rad(theta))
+                    self.points[:][i][1] = self.x[i] * np.sin(np.deg2rad(theta))
                     
                     # Add intensities
                     column_intensities = self.get_column_intensities(self.current, angle)
-                    points[:][i][3] = column_intensities[i]
+                    self.points[:][i][3] = column_intensities[i]
 
                     # Add rgb
                     # points[:][i][4] = column_intensities[i]
                 # Convert the points data to a byte string and assign it to the point cloud message
-                pointcloud_msg.data = points.tobytes()
+                self.pointcloud_msg.data = self.points.tobytes()
+                self.pointcloud_msg.header.stamp = rospy.Time.now()
                 
-                self.pub_pcl.publish(pointcloud_msg)
+                self.pub_pcl.publish(self.pointcloud_msg)
 
     def get_middle_row_intensities(self,image):
         # Get the dimensions of the image
         # Calculate the index of the middle column
         middle_row_index = self.height // 2
-
-    # Retrieve the intensities from the middle row
+        # Retrieve the intensities from the middle row
         middle_row_intensities = image[middle_row_index, :]
-
         return middle_row_intensities
     
     def get_column_intensities(self, image, column_index):
